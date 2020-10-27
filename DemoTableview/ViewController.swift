b5b83b208c72068854e3f902bfcb478c9e5295b6
linhdevios@gmail.com <linhdevios@gmail.com>
2020-10-27 16:48:55 -0700

    Initial Commit

---
+++ DemoTableview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoTableview
+//
+//  Created by admin on 10/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
